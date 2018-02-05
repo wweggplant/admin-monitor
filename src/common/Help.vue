43e638700ddf0c936267f4fc19ca2fca7d9d4a00
wangjojo <weiainijiujiu@126.com>
2018-02-05 16:06:44 +0800

    help组件,显示版权信息

---
+++ src/common/Help.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-badge class='icon-wrap' v-popover:help>
+        <i class='el-icon-question icon'></i>
+        <el-popover
+            ref='help'
+            placement='bottom'
+            width='300'
+            trigger='click'>
+            版权信息:XXXXXXX
+        </el-popover>
+    </el-badge>
+</template>
+<script>
+export default {
+  name: 'vue',
+  data () {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style lang='less'>
+
+</style>
